7c6d63771b30f29c48de047de946a5ef1f3800e1
femipraize <felixogbangwor@gmail.com>
2023-01-22 20:19:12 +0100

    changes

--- introduction.js
+++ introduction.js
@@ -12,12 +12,20 @@ let mesage = "List of colors"
 let colorArray = ['Orange', 'Red', 'Indigo'];
 displayColors(mesage, ...colorArray)
 
-function createPerson(firstName, lastName){
+function createPerson(firstName, lastName, age){
     let fullName = firstName + " " + lastName;
-    return {firstName, lastName, fullName}
+    return {
+        firstName, 
+        lastName, 
+        fullName,
+        isSenior: function(){
+            return age>60;
+        }
+}
 }
 
-let p = createPerson("Femi", "Ogbangwor");
+let p = createPerson("Femi", "Ogbangwor", 70);
 console.log(p.firstName);
 console.log(p.lastName);
 console.log(p.fullName);
+console.log(p.isSenior)
