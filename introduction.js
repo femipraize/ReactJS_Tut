0c35ce7c02f5560bb5a7d117f6a30037896bbc06
femipraize <felixogbangwor@gmail.com>
2023-01-22 20:08:17 +0100

    Object literals

--- introduction.js
+++ introduction.js
@@ -11,3 +11,13 @@ let mesage = "List of colors"
 
 let colorArray = ['Orange', 'Red', 'Indigo'];
 displayColors(mesage, ...colorArray)
+
+function createPerson(firstName, lastName){
+    let fullName = firstName + " " + lastName;
+    return {firstName, lastName, fullName}
+}
+
+let p = createPerson("Femi", "Ogbangwor");
+console.log(p.firstName);
+console.log(p.lastName);
+console.log(p.fullName);
