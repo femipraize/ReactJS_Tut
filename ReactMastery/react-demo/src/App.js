50a2b34426a2bfe4f70c05dd1249c4e12459f890
femipraize <felixogbangwor@gmail.com>
2023-01-28 13:54:26 +0100

    working with react fragment

--- ReactMastery/react-demo/src/App.js
+++ ReactMastery/react-demo/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 
 const App = () =>{
-  const name = 'Femz'
+  const name = null
   const isNameShowing = false;
   return (
     <div className="App">
      <h1>Hello, {isNameShowing? name: 'Boss man'}!</h1>
+     {name ? (
+      <>
+        <h1> {name}</h1>
+      </>
+     ) : (
+      <>
+        <h1> Bug Test</h1>
+        <h2> There No bug</h2>
+      </>
+     ) }
     </div>
   );
 }
