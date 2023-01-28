5bba0e35642e49877a7a27321c2ff40d5a478d9b
femipraize <felixogbangwor@gmail.com>
2023-01-28 18:58:31 +0100

    working with props in react

--- ReactMastery/react-demo/src/App.js
+++ ReactMastery/react-demo/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
-const Person = () => {
+const Person = (props) => {
   return (
     <>
-    <h1> Name: Femi</h1>
-    <h2> last Name : Ogbangwor</h2>
-    <h2> Age: 32</h2>
+    <h1> Name: {props.name}i</h1>
+    <h2> last Name : {props.lastName}</h2>
+    <h2> Age: {props.age}</h2>
+    <h2> Gender:{props.gender}</h2>
+    
     </>
   )
 }
@@ -13,7 +15,12 @@ const Person = () => {
 const App = () =>{
   return (
     <div className="App">
-      <Person />
+      <Person  name='Femi' lastName='Ogbangwor' age='32' gender='male'/>
+      <Person  name='Felicia' lastName='Ogbangwor' age='37' gender='female'/>
+      <Person  name='Abraham' lastName='Ariyo' age='31' gender='male'/>
+      <Person  name='Grace' lastName='Ogbangwor' age='22' gender='female'/>
+      
+
     </div>
   );
 }
