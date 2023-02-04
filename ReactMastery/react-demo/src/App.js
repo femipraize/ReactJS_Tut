13c848fb52362afe2773dbe2432ece121b185622
femipraize <felixogbangwor@gmail.com>
2023-02-04 13:42:07 +0100

    working with use effect

--- ReactMastery/react-demo/src/App.js
+++ ReactMastery/react-demo/src/App.js
@@ -1,9 +1,12 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 
 const App = () =>{
   const [counter, setCounter] = useState(0);
+  useEffect(() =>{
+    alert('you change the counter')
+  }, [counter])
   return (
     <div className="App">
     <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
