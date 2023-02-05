abe8590aa7525599900f4694a60cbcc988ead15f
femipraize <felixogbangwor@gmail.com>
2023-02-05 13:16:48 +0100

    working with use effect

--- ReactMastery/movie-world/src/App.js
+++ ReactMastery/movie-world/src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
-
-// 13cf17c9
+const API_URL ='http://www.omdbapi.com?apikey=13cf17c9'
 
 function App() {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.search);
+  }
+
+  useEffect(() =>{
+    searchMovies('spiderman')
+  }, [])
   return (
     <h1>Web App</h1>
   );
 }
 
 export default App;
+
